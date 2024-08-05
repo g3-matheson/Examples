b9c3838b5430954a59e12a48d7a653fdc1e69c48
Kat Matheson <g3.matheson@gmail.com>
2024-08-05 16:27:55 -0400

    spacing

--- Top K Words.java
+++ Top K Words.java
@@ -17,32 +17,32 @@ public class Solution {
 	private static void topKFrequent(String[] words, int k){
 		
 		Map<String, Integer> hm = new HashMap<String, Integer>();
-
-        for(String word : words){
-            if(!hm.containsKey(word)){
-                hm.put(word, 1);
-            } else {
-                hm.put(word, hm.get(word) + 1);
-            }
-        }
-
-        List<String> list = new ArrayList<>(hm.keySet());
-
-        Collections.sort(list, new Comparator<String>(){
-            @Override
-            public int compare(String s1, String s2){
-                int freq1 = hm.get(s1);
-                int freq2 = hm.get(s2);
-	            if(freq1 == freq2){
-	                return s1.compareTo(s2);
-	            } 
-	            else return freq2 - freq1;
-            }
-    	});
-
-        for(int i = 0; i < k; i++)
-        {
-        	System.out.print("\"" + list.get(i) + "\",");
-        }
+	
+			for(String word : words){
+				if(!hm.containsKey(word)){
+					hm.put(word, 1);
+				} else {
+					hm.put(word, hm.get(word) + 1);
+				}
+			}
+	
+			List<String> list = new ArrayList<>(hm.keySet());
+	
+			Collections.sort(list, new Comparator<String>(){
+				@Override
+				public int compare(String s1, String s2){
+					int freq1 = hm.get(s1);
+					int freq2 = hm.get(s2);
+					if(freq1 == freq2){
+						return s1.compareTo(s2);
+					} 
+					else return freq2 - freq1;
+				}
+			});
+	
+			for(int i = 0; i < k; i++)
+			{
+				System.out.print("\"" + list.get(i) + "\",");
+			}
 	}
 }
