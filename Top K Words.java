61dec72754a13d6abcc5882f370478e90cccb361
Kat Matheson <g3.matheson@gmail.com>
2024-08-05 16:28:19 -0400

    spacing

--- Top K Words.java
+++ Top K Words.java
@@ -18,31 +18,31 @@ public class Solution {
 		
 		Map<String, Integer> hm = new HashMap<String, Integer>();
 	
-			for(String word : words){
-				if(!hm.containsKey(word)){
-					hm.put(word, 1);
-				} else {
-					hm.put(word, hm.get(word) + 1);
-				}
+		for(String word : words){
+			if(!hm.containsKey(word)){
+				hm.put(word, 1);
+			} else {
+				hm.put(word, hm.get(word) + 1);
 			}
-	
-			List<String> list = new ArrayList<>(hm.keySet());
-	
-			Collections.sort(list, new Comparator<String>(){
-				@Override
-				public int compare(String s1, String s2){
-					int freq1 = hm.get(s1);
-					int freq2 = hm.get(s2);
-					if(freq1 == freq2){
-						return s1.compareTo(s2);
-					} 
-					else return freq2 - freq1;
-				}
-			});
-	
-			for(int i = 0; i < k; i++)
-			{
-				System.out.print("\"" + list.get(i) + "\",");
+		}
+
+		List<String> list = new ArrayList<>(hm.keySet());
+
+		Collections.sort(list, new Comparator<String>(){
+			@Override
+			public int compare(String s1, String s2){
+				int freq1 = hm.get(s1);
+				int freq2 = hm.get(s2);
+				if(freq1 == freq2){
+					return s1.compareTo(s2);
+				} 
+				else return freq2 - freq1;
 			}
+		});
+
+		for(int i = 0; i < k; i++)
+		{
+			System.out.print("\"" + list.get(i) + "\",");
+		}
 	}
 }
